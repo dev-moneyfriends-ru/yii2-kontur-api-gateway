bac6aa2b715e4791aa3e85a1da170d62533e1fed
itily <it.ilyha@gmail.com>
2023-01-09 10:35:23 +0300

    Feature: новый метод lessee

--- src/responses/lessee/items/LesseeTermination.php
+++ src/responses/lessee/items/LesseeTermination.php
@@ -4,13 +4,39 @@ declare(strict_types=1);
 
 namespace mfteam\kontur\responses\lessee\items;
 
+use mfteam\kontur\responses\AbstractBaseItem;
+
 /**
- *
+ * Информация о завершении
  *
  * Class LesseeTermination
  * @package mfteam\kontur\responses\lessee\items
  */
-class LesseeTermination
+class LesseeTermination extends AbstractBaseItem
 {
+    /**
+     * @var string|null
+     */
+    protected $date;
 
+    /**
+     * @var string|null
+     */
+    protected $reason;
+
+    /**
+     * @return string|null
+     */
+    public function getDate(): ?string
+    {
+        return $this->date;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getReason(): ?string
+    {
+        return $this->reason;
+    }
 }
--- src/responses/lessee/items/Lessor.php
+++ src/responses/lessee/items/Lessor.php
@@ -4,13 +4,65 @@ declare(strict_types=1);
 
 namespace mfteam\kontur\responses\lessee\items;
 
+use mfteam\kontur\responses\AbstractBaseItem;
+
 /**
  * Лизингодатель
  *
  * Class Lessor
  * @package mfteam\kontur\responses\lessee\items
  */
-class Lessor
+class Lessor extends AbstractBaseItem
 {
+    /**
+     * @var string|null
+     */
+    protected $name;
 
+    /**
+     * @var string|null
+     */
+    protected $inn;
+
+    /**
+     * @var string|null
+     */
+    protected $ogrn;
+
+    /**
+     * @var string|null
+     */
+    protected $country;
+
+    /**
+     * @return string|null
+     */
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getInn(): ?string
+    {
+        return $this->inn;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getOgrn(): ?string
+    {
+        return $this->ogrn;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getCountry(): ?string
+    {
+        return $this->country;
+    }
 }
